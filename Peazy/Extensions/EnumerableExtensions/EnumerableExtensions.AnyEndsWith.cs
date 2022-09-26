b7da33cf65101946c3f2637c285109811db70d7d
Jon Are Wisting <jon.are.wisting@gmail.com>
2022-09-26 20:27:36 +0200

    EnumerableExtensions: Renamed files. Added documentation

--- Peazy/Extensions/EnumerableExtensions/EnumerableExtensions.AnyEndsWith.cs
+++ Peazy/Extensions/EnumerableExtensions/EnumerableExtensions.AnyEndsWith.cs
@@ -6,28 +6,47 @@ namespace Peazy.Extensions
 {
     public static partial class EnumerableExtensions
     {
+        /// <summary>
+        /// Check if any item in an enumerable ends with value. Compares values with <see cref="StringComparison.Ordinal"/>
+        /// </summary>
+        /// <param name="source">Source to seek for value</param>
+        /// <param name="value">Value to seek for</param>
+        /// <returns>Returns true if <paramref name="value"/> is found at end of any value in <paramref name="source"/>, else returns false</returns>
         public static bool AnyEndsWith(this IEnumerable<string> source, string value)
             => EnumerableHelpers.AnyEndsWith(source, value, StringComparison.Ordinal, StringNullStrategy.Skip);
 
+        /// <summary>
+        /// Check if any item in an enumerable ends with value
+        /// </summary>
+        /// <param name="source">Source to seek for value</param>
+        /// <param name="value">Value to seek for</param>
+        /// <param name="comparisonType">Comparison type to use when seeking for <paramref name="value"/> in <paramref name="source"/></param>
+        /// <returns>Returns true if <paramref name="value"/> is found at end of any value in <paramref name="source"/>, else returns false</returns>
         public static bool AnyEndsWith(this IEnumerable<string> source, string value, StringComparison comparisonType)
             => EnumerableHelpers.AnyEndsWith(source, value, comparisonType, StringNullStrategy.Skip);
 
-        public static bool AnyEndsWith(this IEnumerable<string> source, string value, StringNullStrategy nullStrategy)
-            => EnumerableHelpers.AnyEndsWith(source, value, StringComparison.Ordinal, nullStrategy);
-
-        public static bool AnyEndsWith(this IEnumerable<string> source, string value, StringComparison comparisonType, StringNullStrategy nullStrategy)
-            => EnumerableHelpers.AnyEndsWith(source, value, comparisonType, nullStrategy);
-
+        /// <summary>
+        /// Check if any item in an enumerable ends with value
+        /// </summary>
+        /// <param name="source">Source to seek for value</param>
+        /// <param name="selector">Select what value of an item get checked against <paramref name="value"/></param>
+        /// <param name="value">Value to seek for</param>
+        /// <typeparam name="T">Type of enumerable</typeparam>
+        /// <returns>Returns true if <paramref name="value"/> is found at end of value in <paramref name="source"/>, given by <paramref name="selector"/>, else returns false</returns>
         public static bool AnyEndsWith<T>(this IEnumerable<T> source, Func<T, string> selector, string value)
             => EnumerableHelpers.AnyEndsWith(source, selector, value, StringComparison.Ordinal, StringNullStrategy.Skip);
 
+        /// <summary>
+        /// Check if any item in an enumerable ends with value
+        /// </summary>
+        /// <param name="source">Source to seek for value</param>
+        /// <param name="selector">Select what value of an item get checked against <paramref name="value"/></param>
+        /// <param name="value">Value to seek for</param>
+        /// /// <param name="comparisonType">Comparison type to use when seeking for <paramref name="value"/> in <paramref name="source"/></param>
+        /// <typeparam name="T">Type of enumerable</typeparam>
+        /// <returns>Returns true if <paramref name="value"/> is found at end of value in <paramref name="source"/>, given by <paramref name="selector"/>, else returns false</returns>
         public static bool AnyEndsWith<T>(this IEnumerable<T> source, Func<T, string> selector, string value, StringComparison comparisonType)
             => EnumerableHelpers.AnyEndsWith(source, selector, value, comparisonType, StringNullStrategy.Skip);
 
-        public static bool AnyEndsWith<T>(this IEnumerable<T> source, Func<T, string> selector, string value, StringNullStrategy nullStrategy)
-            => EnumerableHelpers.AnyEndsWith(source, selector, value, StringComparison.Ordinal, nullStrategy);
-
-        public static bool AnyEndsWith<T>(this IEnumerable<T> source, Func<T, string> selector, string value, StringComparison comparisonType, StringNullStrategy nullStrategy)
-            => EnumerableHelpers.AnyEndsWith(source, selector, value, comparisonType, nullStrategy);
     }
 }
--- Peazy/Extensions/EnumerableExtensions/EnumerableExtensions.Select.cs
+++ Peazy/Extensions/EnumerableExtensions/EnumerableExtensions.Select.cs
@@ -6,13 +6,27 @@ namespace Peazy.Extensions
 {
     public static partial class EnumerableExtensions
     {
-        public static IEnumerable<E> SelectWhere<T, E>(this IEnumerable<T> elements, Func<T, E> selector, Predicate<T> predicate)
-                => EnumerableHelpers.SelectWhere(elements, selector, predicate);
+        /// <summary>
+        /// Project <paramref name="source"/> items to another type, provided item is qualified by <paramref name="predicate"/>
+        /// </summary>
+        /// <param name="source">Source of items to project</param>
+        /// <param name="selector">Projecting function</param>
+        /// <param name="predicate">Qualify item for projection</param>
+        /// <typeparam name="T">Type of items to project</typeparam>
+        /// <typeparam name="E">Type of projected items</typeparam>
+        /// <returns>Returns a new enumerable containing projected items</returns>
+        public static IEnumerable<E> SelectWhere<T, E>(this IEnumerable<T> source, Func<T, E> selector, Predicate<T> predicate)
+                => EnumerableHelpers.SelectWhere(source, selector, predicate);
 
-        public static List<E> SelectToList<T, E>(this IEnumerable<T> elements, Func<T, E> selector)
-                => EnumerableHelpers.SelectToList(elements, selector);
-
-        public static List<E> SelectToListWhere<T, E>(this IEnumerable<T> elements, Func<T, E> selector, Predicate<T> predicate)
-                => EnumerableHelpers.SelectToListWhere(elements, selector, predicate);
+        /// <summary>
+        /// Project <paramref name="source"/> items to another type and return as List
+        /// </summary>
+        /// <param name="source">Source of items to project</param>
+        /// <param name="selector">Projecting function</param>
+        /// <typeparam name="T">Type of items to project</typeparam>
+        /// <typeparam name="E">Type of projected items</typeparam>
+        /// <returns>Returns a new list containing projected items</returns>
+        public static List<E> SelectToList<T, E>(this IEnumerable<T> source, Func<T, E> selector)
+                => EnumerableHelpers.SelectToList(source, selector);
     }
 }
