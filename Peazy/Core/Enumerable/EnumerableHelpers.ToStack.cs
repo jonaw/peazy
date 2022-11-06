b7cbe0d64c456267c4b50524f444ca7c47202f0a
Jon Are Wisting <jon.are.wisting@gmail.com>
2022-11-06 11:27:43 +0100

    EnumerableExtensions: Added extensions to create new stack from enumerable

---
+++ Peazy/Core/Enumerable/EnumerableHelpers.ToStack.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace Peazy.Core
+{
+    internal static partial class EnumerableHelpers
+    {
+        /// <summary>
+        /// Create a new Stack from an enumerable
+        /// </summary>
+        /// <param name="source">Source items</param>
+        /// <typeparam name="T">The type of elements in enumerable and stack</typeparam>
+        /// <returns>Returns a new stack containing elements from enumerable</returns>
+		public static Stack<T> ToStack<T>(IEnumerable<T> source)
+        {
+            return new Stack<T>(source);
+        }
+    }
+}
